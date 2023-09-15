8e64b269241a7c8e7434b765dde6b721e82f7ecd
Ericesposito <eespo93@optonline.net>
2023-09-14 23:12:25 -0400

    finished up module 3 - starting project

--- code/01-starting-setup/src/App.js
+++ code/01-starting-setup/src/App.js
@@ -1,7 +1,43 @@
-function App() {
+// import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
+import React from 'react';
+
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, `Let's get started!`),
+  //   React.createElement('p', {}, `This is also visible`),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible</p>
+      <Expenses items={expenses}/>
     </div>
   );
 }
